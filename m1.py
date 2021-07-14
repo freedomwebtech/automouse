318139f514f4d698233b15379de5eb083a8990bc
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-07-14 18:35:02 +0530

    Add files via upload

---
+++ m1.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+pyautogui.click(1301, 50)
+pyautogui.click(16, 14)
+time.sleep(1)
+pyautogui.click(85, 116)
+time.sleep(1)
+pyautogui.click(282, 163)
---
+++ m2.py
@@ -0,0 +1,14 @@
+import pyautogui as pg
+import time
+pg.click(144,13)
+time.sleep(1)
+pg.click(287, 248)
+time.sleep(1)
+
+
+#pg.click(289, 248)
+pg.typewrite(["enter"])
+time.sleep(1)
+pg.typewrite("ifconfig")
+time.sleep(1)
+pg.typewrite(["enter"])
